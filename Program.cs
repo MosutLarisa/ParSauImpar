40b3a976b5eebdafdf88bd01e223a497e9f2fa32
MosutLarisa <146415153+MosutLarisa@users.noreply.github.com>
2023-10-13 14:55:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ParSauImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Console.WriteLine(a);
+        }
+    }
+}
